52a9af27d8917962e49222a7cd0fa625eb0648fa
Daniel P <daniel@pont.in>
2021-03-26 15:40:08 +0000

    Remove unneeded comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use human_format::Formatter;
 use log::{error, info};
-//use reqwest::Result;
+
 use serde::{de::value::StrDeserializer, Deserialize};
 use serenity::{
     async_trait,
@@ -18,11 +18,7 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
+
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content.to_lowercase().starts_with("!vacced") {
 
@@ -49,12 +45,6 @@ impl EventHandler for Handler {
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
